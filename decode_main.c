baedd803aeb8800d987a9e43b034fe42128d21da
Blake Ramsdell <blaker@gmail.com>
2022-05-29 17:41:43 -0700

    Read instructions from stdin in decode_main

--- decode_main.c
+++ decode_main.c
@@ -1,12 +1,20 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "decode.h"
 
 int main(void)
 {
-    char output[128];
-    decode_one_instruction(0x3c025537, output, sizeof(output));
-    printf("%s\n", output);
+    char buffer[128];
+    uint32_t instruction;
+
+    while (fgets(buffer, sizeof(buffer), stdin))
+    {
+        instruction = strtol(buffer, NULL, 16);
+
+        decode_one_instruction(instruction, buffer, sizeof(buffer));
+        printf("%08x %s\n", instruction, buffer);
+    }
 }
