56659963f767615106a67a8aa1c4e597688a2fae
Blake Ramsdell <blaker@gmail.com>
2022-06-04 18:54:35 -0700

    Include address in testdata

--- decode_main.c
+++ decode_main.c
@@ -2,19 +2,28 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "decode.h"
 
 int main(void)
 {
     char buffer[128];
+    uint32_t address;
     uint32_t instruction;
+    const char* instruction_ptr;
 
     while (fgets(buffer, sizeof(buffer), stdin))
     {
-        instruction = strtol(buffer, NULL, 16);
+        address = strtol(buffer, NULL, 16);
+        instruction_ptr = strchr(buffer, ':');
+        if (!instruction_ptr)
+        {
+            continue;
+        }
+        instruction = strtol(instruction_ptr + 1, NULL, 16);
 
         decode_one_instruction(instruction, buffer, sizeof(buffer));
-        printf("%08x %s\n", instruction, buffer);
+        printf("%4x:\t%08x %s\n", address, instruction, buffer);
     }
 }
